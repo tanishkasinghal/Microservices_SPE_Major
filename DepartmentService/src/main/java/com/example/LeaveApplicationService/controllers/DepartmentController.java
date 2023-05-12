cab44fe00832d7c9d59e29f5c0c42ac71cd884f6
Tanishka <tanishka@pop-os.localdomain>
2023-05-12 19:59:48 +0530

    Looging added

--- DepartmentService/src/main/java/com/example/LeaveApplicationService/controllers/DepartmentController.java
+++ DepartmentService/src/main/java/com/example/LeaveApplicationService/controllers/DepartmentController.java
@@ -3,6 +3,8 @@ package com.example.LeaveApplicationService.controllers;
 import com.example.LeaveApplicationService.entities.Department;
 import com.example.LeaveApplicationService.services.DepartmentService;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +17,24 @@ import java.util.List;
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
-
+    private static final Logger logger = LogManager.getLogger(DepartmentController.class);
     @PostMapping("/")
     public ResponseEntity<Department> addDepartment(@Valid @RequestBody Department department){
         Department newDepartment=this.departmentService.addDepartment(department);
+        logger.info("Adding Department");
         return new ResponseEntity<>(newDepartment, HttpStatus.CREATED);
 
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Department> getDepartmentbyId(@PathVariable String id){
+        logger.info("Getting Department by given id");
         return ResponseEntity.ok(this.departmentService.getDepartmentById(id));
     }
 
     @GetMapping("/")
     public ResponseEntity<List<Department>> getAllDepartment(){
-        System.out.println("jn");
+        logger.info("Getting list of all Departments");
         return ResponseEntity.ok(this.departmentService.getAllDepartments());
     }
 }
--- EmployeeService/src/main/java/com/example/EmployeeService/controllers/EmployeeController.java
+++ EmployeeService/src/main/java/com/example/EmployeeService/controllers/EmployeeController.java
@@ -6,6 +6,8 @@ import com.example.EmployeeService.payload.ApiResponse;
 import com.example.EmployeeService.payload.EmployeeResponse;
 import com.example.EmployeeService.services.EmployeeService;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +20,18 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-
+    private static final Logger logger = LogManager.getLogger(EmployeeController.class);
     @PostMapping("/")
     public ResponseEntity<Employee> addEmployee(@Valid @RequestBody Employee employee){
         Employee newEmployee=this.employeeService.addEmployee(employee);
+        logger.info("Adding Employee");
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 
     @PostMapping("/register")
     public ResponseEntity<Employee> registerUser(@RequestBody Employee employee){
         Employee newUser=this.employeeService.registerNewUser(employee);
+        logger.info("Registering Employee");
         return new ResponseEntity<Employee>(newUser,HttpStatus.CREATED);
     }
 
@@ -37,18 +41,21 @@ public class EmployeeController {
                                                             @RequestParam(value = "sortBy",defaultValue = AppConstants.SORT_BY,required = false) String sortBy,
                                                             @RequestParam(value = "sortDir",defaultValue = AppConstants.SORT_DIR,required = false) String sortDir){
         EmployeeResponse employeeResponse=this.employeeService.getAllEmployee(pageNumber,pageSize,sortBy,sortDir);
+        logger.info("Getting All Employee");
         return new ResponseEntity<EmployeeResponse>(employeeResponse,HttpStatus.OK);
     }
 
     @GetMapping("/department/{deptId}/")
     public ResponseEntity<List<Employee>> getEmployeesByDepartmentId(@PathVariable String deptId){
         List<Employee> employees=this.employeeService.getEmployeeByDepartment(deptId);
+        logger.info("Getting Department By id");
         return new ResponseEntity<List<Employee>>(employees,HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Employee> updateEmployee(@Valid @RequestBody Employee employee,@PathVariable String id){
         Employee updatedEmployeeDTO=this.employeeService.updateEmployeeDetails(employee,id);
+        logger.info("Update Employee");
         return ResponseEntity.ok(updatedEmployeeDTO);
     }
 
@@ -56,12 +63,14 @@ public class EmployeeController {
     public ResponseEntity<ApiResponse> deleteEmployee(@PathVariable("id") String id){
         this.employeeService.deleteEmployee(id);
         ApiResponse response=ApiResponse.builder().message("User Deleted Successfully").success(true).status(HttpStatus.OK).build();
+        logger.info("Deleting Employee");
         return new ResponseEntity<ApiResponse>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeebyId(@PathVariable String id){
         Employee employee=this.employeeService.getEmployeeById(id);
+        logger.info("Getting Employee by Id");
         return new ResponseEntity<Employee>(employee,HttpStatus.OK);
     }
 
@@ -69,6 +78,7 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> searchEmployeeByName(
             @PathVariable("keyword") String keyword){
         List<Employee> employee=this.employeeService.serachEmployee(keyword);
+        logger.info("Searching Employee");
         return new ResponseEntity<List<Employee>>(employee,HttpStatus.OK);
     }
 }
